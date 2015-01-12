f613b34c15ad1c354460828ae862d8be808f6cc3
Andrei Halanay <halanay@fmi.unibuc.ro>
2015-01-13 00:39:11 +0200

    Start

--- src/Main.hs
+++ src/Main.hs
@@ -5,6 +5,8 @@ latex m = putStrLn $ latexFormat "array" (dispf 2 m)
 m = (3><4)[1..] :: Matrix Double
 --(q,r) = qr m
 
+prim = [[[[i,j,k]| i<- [-3..3],j<- [-3..3],k<- [-3..3]],[[i,j,k]|i<- [-3..3],j<- [-3..3],k<- [-3..3]],[[i,j,k]|i<- [-3..3],j<- [-3..3],k<- [-3..3]]]]
+
 main = do
   disp m
   latex m

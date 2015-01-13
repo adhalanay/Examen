ab74a5198e618702de7674916f07e632f5ba55a6
Andrei Halanay <halanay@fmi.unibuc.ro>
2015-01-13 22:53:08 +0200

    Convert to Double

--- src/Main.hs
+++ src/Main.hs
@@ -4,14 +4,13 @@ import Control.Monad
 disp = putStr . dispf 2
 latex m = putStrLn $ latexFormat "array" (dispf 2 m)
 
-generator :: Int -> Int -> Int -> [Matrix Double]
+generator :: Int -> Int -> Int -> [[[Double]]]
 
-generator s m n = 
+generator s m 0 = []
+generator s m n =
    let xs = replicateM m [-s..s]
-   in  [x,y | x <-xs,y<-xs]
-
-   
+       ys = [[fromIntegral z :: Double | z <- x] | x <- xs]
+   in  generator s m (n-1) ++ [ys]
 
 main = do
-  disp m
-  latex m
+  print $ generator 1 3 2

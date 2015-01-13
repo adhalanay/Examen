d76d95b5d573baa28c3d7035122535b0ba4a9c0d
Andrei Halanay <halanay@fmi.unibuc.ro>
2015-01-13 14:03:46 +0200

    First attempt

--- src/Main.hs
+++ src/Main.hs
@@ -6,8 +6,10 @@ latex m = putStrLn $ latexFormat "array" (dispf 2 m)
 
 generator :: Int -> Int -> Int -> [Matrix Double]
 
-generator s m n =
-   xs = replicateM m [-s..s]
+generator s m n = 
+   let xs = replicateM m [-s..s]
+   in  [x,y | x <-xs,y<-xs]
+
    
 
 main = do

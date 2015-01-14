0e4819650da66794944c9243d9a488b74f31cd9c
Andrei Halanay <halanay@fmi.unibuc.ro>
2015-01-15 01:14:03 +0200

    RandomList

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,7 @@
 import Numeric.LinearAlgebra
 import Control.Monad
+import RandomList
+
 
 disp = putStr . dispf 2
 latex m = putStrLn $ latexFormat "array" (dispf 2 m)
@@ -8,26 +10,27 @@ generator :: Int -> Int -> Int -> [[[Double]]]
 conversion :: [[[Double]]] -> [Matrix Double]
 selection :: Int->Int->[Matrix Double] -> [Matrix Double]
 
-generator s m 1 = 
+generator s m 1 =
 	let xs = replicateM m [-s..s]
 	    ys = [[fromIntegral z :: Double | z <- x] | x <- xs]
-    in  [ys]
+	    zs = permute ys
+        in  [ys]
 generator s m 2 =
    let xs = replicateM m [-s..s]
        ys = [[fromIntegral z :: Double | z <- x] | x <- xs]
-    in  [[x]++[y] | x<-ys,y<-ys]
+    in  [[x]++[y] | x<- ys,y<- ys]
 generator s m n =
 	let xs = replicateM m [-s..s]
 	    ys = [[fromIntegral z :: Double | z <- x] | x <- xs]
 	    zs = generator s m (n-1)
-	in [x++[y]| x<- zs,y<-ys]
+	in [x++[y]| x<- zs,y<- ys]
 
-conversion = map fromLists 
+conversion = map fromLists
 
 selection r1 r2 = filter (\a -> rank a >= r1 && rank a <=r2)
 
 main = do
     let xs = generator 2 5 3
-        ys = take 10 $ selection 1 2 (conversion xs)
+        ys = selection 1 2 (conversion xs)
         l = length ys
-    print ys 
+    print l

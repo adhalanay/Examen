e99192a16472f61e2262c33c4e714d329346f6f6
Andrei Halanay <halanay@fmi.unibuc.ro>
2015-01-15 23:44:44 +0200

    Shuffle

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,21 @@
 import Numeric.LinearAlgebra
 import Control.Monad
-import RandomList
+import Control.Monad.Random
+--import System.Random.Mersenne.Pure64 (newPureMT)
+--import RandomList
 
+choose _ [] = error "choose: index out of range"
+choose 0 (x:xs) = (x, xs)
+choose i (x:xs) = let (y, ys) = choose (i - 1) xs in (y, x:ys)
+
+shuffle :: Int -> Int -> [a] -> [a]
+shuffle seed 0 _ = []
+shuffle seed len xs =
+    let
+       n = fst $ randomR (0, len - 1) (mkStdGen seed)
+       (y, ys) = choose n xs
+       ys' = shuffle seed (len - 1) ys
+    in y:ys'
 
 disp = putStr . dispf 2
 latex m = putStrLn $ latexFormat "array" (dispf 2 m)
@@ -9,28 +23,40 @@ latex m = putStrLn $ latexFormat "array" (dispf 2 m)
 generator :: Int -> Int -> Int -> [[[Double]]]
 conversion :: [[[Double]]] -> [Matrix Double]
 selection :: Int->Int->[Matrix Double] -> [Matrix Double]
+disp_ls :: [Matrix Double] -> IO()
+
+disp_ls [] = disp $ fromLists [[]]
+disp_ls [x] = disp x
+disp_ls (x:xs) = do
+      disp x
+      disp_ls xs
 
 generator s m 1 =
-	let xs = replicateM m [-s..s]
-	    ys = [[fromIntegral z :: Double | z <- x] | x <- xs]
-	    zs = permute ys
-        in  [ys]
+           let xs = replicateM m [-s..s]
+               ys = [[fromIntegral z :: Double | z <- x] | x <- xs]
+               zs = shuffle 18495758398490 (length ys) ys
+           in [zs]
 generator s m 2 =
-   let xs = replicateM m [-s..s]
-       ys = [[fromIntegral z :: Double | z <- x] | x <- xs]
-    in  [[x]++[y] | x<- ys,y<- ys]
+           let xs = replicateM m [-s..s]
+               ys = [[fromIntegral z :: Double | z <- x] | x <- xs]
+               ys1 = shuffle 10094748500790 (length ys) ys
+               ys2 = shuffle 963547459682435 (length ys1) ys1
+           in [[x]++[y] | x<- ys1,y<- ys2]
 generator s m n =
-	let xs = replicateM m [-s..s]
-	    ys = [[fromIntegral z :: Double | z <- x] | x <- xs]
-	    zs = generator s m (n-1)
-	in [x++[y]| x<- zs,y<- ys]
+      let xs = replicateM m [-s..s]
+          ys = [[fromIntegral z :: Double | z <- x] | x <- xs]
+          ys1 = shuffle 5302946104563828 (length ys) ys
+          zs = generator s m (n-1)
+          zs1 = shuffle 674857894620857 (length zs) zs
+      in [x++[y]| x<- zs1,y<- ys1]
 
 conversion = map fromLists
 
 selection r1 r2 = filter (\a -> rank a >= r1 && rank a <=r2)
 
 main = do
-    let xs = generator 2 5 3
-        ys = selection 1 2 (conversion xs)
-        l = length ys
-    print l
+      let xs = generator 2 5 4
+          ys = take 500 $ selection 2 2 (conversion xs)
+          zs = shuffle 89309170391038948 (length ys) ys
+          zss = take 10 zs
+      disp_ls zss
--- src/RandomList.hs
+++ src/RandomList.hs
@@ -1,6 +1,7 @@
 --- apfelmus 2009. This code is hereby released into public domain.
 module RandomList where
 
+import System.Random
 import Control.Monad.Random
 import Control.Monad
 
@@ -9,11 +10,24 @@ type R a = Rand StdGen a
     -- List returning elements in random order
 type RandomList a = R [a]
 
+print_rl :: RandomList [Integer] -> IO()
+print_rl rl = do
+     g <- getStdGen
+     let xs = evalRand rl g
+     putStrLn (show xs)
+
+extract :: RandomList a -> [a]
+
+extract rxs = do
+      let g=mkStdGen 1000
+      evalRand rxs g
+
 empty :: RandomList a
 empty = return []
 
 singleton :: a -> RandomList a
 singleton x = return [x]
+
 
     -- Fair merge of random lists
 merge :: RandomList a -> RandomList a -> RandomList a
@@ -21,10 +35,10 @@ merge rxs rys = do
         xs <- rxs
         ys <- rys
         merge' (length xs, xs) (length ys, ys)
-    where
-    merge' (0 , [])   (_ , ys)   = return ys
-    merge' (_ , xs)   (0 , [])   = return xs
-    merge' (nx, x:xs) (ny, y:ys) = do
+      where
+      merge' (0 , [])   (_ , ys)   = return ys
+      merge' (_ , xs)   (0 , [])   = return xs
+      merge' (nx, x:xs) (ny, y:ys) = do
         k <- getRandomR (1,nx+ny)   -- selection weighted by size
         if k <= nx
             then (x:) `liftM` ((nx-1, xs) `merge'` (ny, y:ys))
@@ -37,4 +51,5 @@ permute = fromList
     fromList []  = empty
     fromList [x] = singleton x
     fromList xs  = (fromList l) `merge` (fromList r)
-        where (l,r) = splitAt (length xs `div` 2) xs----------------------------------------------------------------------------
+        where (l,r) = splitAt (length xs `div` 2) xs
+        ----------------------------------------------------------------------------

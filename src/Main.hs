6ee568c515b979fb214296f48eea8f9f1ff0592d
Andrei Halanay <halanay@fmi.unibuc.ro>
2015-01-17 01:41:20 +0200

    Generated

--- src/Main.hs
+++ src/Main.hs
@@ -1,9 +1,9 @@
 import Numeric.LinearAlgebra
 import Control.Monad
-import Control.Monad.Random
+--import Control.Monad.Random
 --import System.Random.Mersenne.Pure64 (newPureMT)
 --import RandomList
-import System.Random
+--import System.Random
 import System.Random.Shuffle
 
 {-choose _ [] = error "choose: index out of range"
@@ -33,29 +33,33 @@ disp_ls (x:xs) = do
       disp x
       disp_ls xs
 
-generator s m 1 = 
-           let xs = replicateM m [-s..s]
+good :: Eq a => [a] -> Bool
+good [] = False
+good [x] = False
+good [x,y] = x/=y
+good (x:xs) = x/= (head xs) && good xs
+
+generator s m 1 =
+            let  xs = replicateM m [-s..s]
+                 ys = [[fromIntegral z :: Double | z <- x] | x <- xs]
+            in [ys]
+generator s m 2 =
+           let xs = [x | x<- replicateM m [-s..s], good x ]
                ys = [[fromIntegral z :: Double | z <- x] | x <- xs]
-           in do zs <- shuffleM ys
-                 [zs]
-generator s m 2 = do
-           let xs = replicateM m [-s..s]
-               ys = [[fromIntegral z :: Double | z <- x] | x <- xs]
-           ys1 <- shuffleM ys
-           ys2 <- shuffleM ys1
-           [[x]++[y] | x<- ys1,y<- ys2]
-generator s m n = do
+               z = take m $ repeat 0
+           in [[x]++[y] | x<- ys,y<- ys, x/=y && x/=z && y/=z]
+generator s m n =
       let xs = replicateM m [-s..s]
           ys = [[fromIntegral z :: Double | z <- x] | x <- xs]
           zs = generator s m (n-1)
-      ys1 <-shuffleM ys  
-      [x++[y]| x<- zs,y<- ys1]
+      in [x++[y]| x<- zs,y<- ys]
 
 conversion = map fromLists
 
 selection r1 r2 = filter (\a -> rank a >= r1 && rank a <=r2)
 
 main = do
-      let xs = generator 2 5 4
-          ys = take 50 $ selection 2 2 (conversion xs)
-      disp_ls ys
+          let xs = generator 2 5 4
+              ys = take 1000 $ selection 3 3 (conversion xs)
+          zs <- shuffleM ys
+          disp_ls $ take 50 zs
